bd459fb1d698e46dfc4ea8f5a4fa2a15507990c6
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-12-04 16:24:26 -0700

    implement grid and fix synthetic imports

--- app/src/main/java/com/example/minesweeper/MainActivity.kt
+++ app/src/main/java/com/example/minesweeper/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.minesweeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cells: Array<Array<Cell>>
+    lateinit var adapter: GridAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cells = Array(10) {row ->
+            Array(10) {col ->
+                Cell(false, row, col)
+            }
+        }
+
+        adapter = GridAdapter(this, cells.flatten())
+        GameBoard.adapter = adapter
     }
 }
