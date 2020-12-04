4d32e77ee1cdc5bdeceedae75b11ca1895727610
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2020-12-04 16:34:48 -0700

    add the ability to flip tiles by tapping on them

--- app/src/main/java/com/example/minesweeper/MainActivity.kt
+++ app/src/main/java/com/example/minesweeper/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.minesweeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.grid_item.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,5 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         adapter = GridAdapter(this, cells.flatten())
         GameBoard.adapter = adapter
+
+        GameBoard.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, v, position, id ->
+                revealCell(cells.flatten().get(position))
+                v.cell.setImageResource(cells.flatten().get(position).returnImage())
+            }
+    }
+
+    fun revealCell(cell: Cell) {
+        cell.faceUp = true
     }
 }
